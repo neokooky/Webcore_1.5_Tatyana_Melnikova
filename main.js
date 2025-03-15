61a7758d967d22a88b7aa5478f123f290eed5879
Tatyana Melnikova <neonatura01@yandex.ru>
2025-03-15 15:55:52 +0800

    Заменя стилей

--- main.js
+++ main.js
@@ -1,74 +1,72 @@
 /* Swiper */
 
-
 let swiper;
 
-const swiperInit = function() {
-    if (!swiper) {
-        swiper = new Swiper(".brands-swiper", {
-            loop: true,
-            slidesPerView: 'auto',
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-    }
+const swiperInit = function () {
+  if (!swiper) {
+    swiper = new Swiper(".brands__swiper", {
+      loop: true,
+      slidesPerView: "auto",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
 };
 
-const widthTablet = window.matchMedia('(max-width: 767px)');
+const widthTablet = window.matchMedia("(max-width: 767px)");
 
 const destroySwiper = function (event) {
-    if (!event.matches && swiper) {
-        swiper.destroy();
-        swiper = undefined;
-    } else if (event.matches) {
-        swiperInit();
-    }
+  if (!event.matches && swiper) {
+    swiper.destroy();
+    swiper = undefined;
+  } else if (event.matches) {
+    swiperInit();
+  }
 };
 
 destroySwiper(widthTablet);
-widthTablet.addEventListener('change', destroySwiper);
-
+widthTablet.addEventListener("change", destroySwiper);
 
 /* Hiding brands */
 
-
-const toggleButton = document.getElementById('toggle-button');
-const items = document.querySelectorAll('.brands__item');
+const toggleButton = document.getElementById("toggle-button");
+let toggleButtonText = document.querySelector(".button__text");
+const items = document.querySelectorAll(".brands__item");
 
 function hideItems() {
-    if (window.innerWidth < 1016) {
-      items.forEach((item, index) => {
-        if (index >= 6) {
-          item.classList.add('hidden');
-        } else {
-          item.classList.remove('hidden');
-        }
-      });
-    } else {
-      items.forEach((item, index) => {
-        if (index >= 8) {
-          item.classList.add('hidden');
-        } else {
-          item.classList.remove('hidden');
-        }
-      });
-    }
+  if (window.innerWidth < 1016) {
+    items.forEach((item, index) => {
+      if (index >= 6) {
+        item.classList.add("hidden");
+      } else {
+        item.classList.remove("hidden");
+      }
+    });
+  } else {
+    items.forEach((item, index) => {
+      if (index >= 8) {
+        item.classList.add("hidden");
+      } else {
+        item.classList.remove("hidden");
+      }
+    });
+  }
 }
 
 hideItems();
-  
-  window.addEventListener('resize', hideItems);
-  
-  toggleButton.addEventListener('click', function () {
-    if (toggleButton.textContent === 'Показать все') {
-      items.forEach(item => item.classList.remove('hidden'));
-      toggleButton.textContent = 'Скрыть';
-      toggleButton.classList.add('rotate-decor');
-    } else {
-      hideItems();
-      toggleButton.textContent = 'Показать все';
-      toggleButton.classList.remove('rotate-decor');
-    }
-  });
+
+window.addEventListener("resize", hideItems);
+
+toggleButton.addEventListener("click", function () {
+  if (toggleButtonText.textContent === "Показать все") {
+    items.forEach((item) => item.classList.remove("hidden"));
+    toggleButtonText.textContent = "Скрыть";
+    toggleButtonText.classList.add("rotate-decor");
+  } else {
+    hideItems();
+    toggleButtonText.textContent = "Показать все";
+    toggleButtonText.classList.remove("rotate-decor");
+  }
+});
